f4c5a52e5328d195a589585c99a4c800a2ac71b4
backtrackbaba <sai.cosmo@gmail.com>
2017-08-30 17:56:46 +0000

    Done

--- build.py
+++ build.py
@@ -1,14 +1,10 @@
-import operator
-
-
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
+    return sorted(dic.items())
 
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    return list(reversed(sorted(dic.items())))
 
+dic = {1: 20, 3: 40, 4: 30, 2: 10, 0: 8}
+print solution_asc (dic)
+print solution_desc(dic)
